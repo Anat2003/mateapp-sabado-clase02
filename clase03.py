e2529ed18e1930a48046e7888f160b8e0790b362
Anat2003 <81696043+Anat2003@users.noreply.github.com>
2021-04-24 13:15:10 -0500

    Create clase03.py

---
+++ clase03.py
@@ -0,0 +1,20 @@
+a = "uno"
+b = "dos"
+c = a + b
+print(c)
+c = a * 3
+print(c)
+
+
+a= 14
+b= 12
+c= a + b
+print (a, "hola", b , "=", c)
+print (a+b)
+print (a, "hola", b , "=", c)
+
+
+h= "hola"
+i= "="
+
+print(str(a)+ "hola"+ str(b)+ '='+ str(c))
